cddaea1e5e7c6efebdb93ab2b52f7246f5c79456
Sausty45 <36990678+Sausty45@users.noreply.github.com>
2021-07-26 17:45:14 +0200

    Commentated example_app

--- example_app/main.c
+++ example_app/main.c
@@ -8,27 +8,31 @@ static int debug = 0;
 
 int main()
 {
+    // Initialises Vk2D
     vk2d_init(vk2d_init_everything);
 
+    // Creates a window with the given width, height, and title
     vk2d_window* window = vk2d_create_window(1280, 720, "Hello, Vk2D!");
+    // Creates an input system attached to a window
     vk2d_input_system* input = vk2d_create_input_system(window);
 
+    // Initialises the Vk2D renderer
     vk2d_init_renderer(window, debug);
 
-    vk2d_texture* c_language = vk2d_texture_init_from_file("assets/fawful.png");
-    vk2d_sound* music = vk2d_init_sound_from_file("assets/game_sound.mp3", vk2d_audio_format_mp3);
-    vk2d_update_sound_attributes(music);
-
-    vk2d_play_sound(music);
+    // Loads a texture
+    vk2d_texture* fawful_image = vk2d_texture_init_from_file("assets/fawful.png");
 
     int i = 0;
     while (!vk2d_window_should_close(window))
     {
+        // Polls the window's events
         vk2d_window_update(window);
 
+        // Create camera matrices
         vk2d_mat4 projection = vk2d_mat4_identity();
         vk2d_mat4 view = vk2d_mat4_identity();
 
+        // Begins the rendering
         vk2d_renderer_begin_scene(projection, view);
         
         for (float y = -5.0f; y < 5.0f; y += 0.5f)
@@ -39,22 +43,28 @@ int main()
                 vk2d_vec3 pos = { x, y, 0.0f };
                 vk2d_vec3 scale = { 0.15f, 0.15f, 1.0f };
 
-                vk2d_renderer_draw_textured_quad(pos, scale, vk2d_vec3_new(0.0f, 0.0f, 1.0f), i * 0.2f, c_language, color);
+                // Draw a grid of quads with a texture, position, scale, rotation and color
+                vk2d_renderer_draw_textured_quad(pos, scale, vk2d_vec3_new(0.0f, 0.0f, 1.0f), i * 0.2f, fawful_image, color);
             }
         }
         i++;
 
+        // End the scene
         vk2d_renderer_end_scene();
 
+        // Present
         vk2d_renderer_draw();
     }
 
-    vk2d_free_sound(music);
-    vk2d_texture_free(c_language);
+    // Frees the fawful texture and shutdown the renderer
+    vk2d_texture_free(fawful_image);
     vk2d_shutdown_renderer();
 
+    // Frees the input system and the window
     vk2d_free_input_system(input);
     vk2d_quit_window(window);
+
+    // Terminate Vk2D
     vk2d_quit();
     return 0;
 }
